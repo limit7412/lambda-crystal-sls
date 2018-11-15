be723563330a07d98f9425d67ee5454c0a3e4768
qazx7412 <git@newgenerations.im>
2018-11-15 17:11:54 +0900

    きれいに

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # lambdaでcrystalが動かないかと企んだ残骸
-  - mkdir buildfile
-  - docker-compose build
-  - docker-compose up
-  - sls deploy
+  - ./build.sh
--- handler.js
+++ handler.js
@@ -2,35 +2,14 @@
 
 const exec = require('child_process').exec;
 
-module.exports.hello_ruby = (event, context, callback) => {
-  const child = exec('./traveling-ruby/bin/ruby ./src/hello_ruby.rb ' + "'" + JSON.stringify(event) + "'");
-
-  child.stdout.on('data', (result) => {
-    callback(null,result);
-  });
-  child.stderr.on('data', (result) => {
-    callback(result);
-  });
-};
-
 module.exports.hello_crystal = (event, context, callback) => {
   const child = exec('./buildfile/hello_crystal ' + "'" + JSON.stringify(event) + "'");
 
-  child.stdout.on('data', (result) => {
-    callback(null,result);
-  });
   child.stderr.on('data', (result) => {
     callback(result);
   });
-};
-
-module.exports.build_elixir = (event, context, callback) => {
-  const child = exec('./buildfile/build_elixir ' + "'" + JSON.stringify(event) + "'");
-
   child.stdout.on('data', (result) => {
     callback(null,result);
   });
-  child.stderr.on('data', (result) => {
-    callback(result);
-  });
 };
+
