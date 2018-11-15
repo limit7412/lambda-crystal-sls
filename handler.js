3eb9d041d077a33acd48de137fee77bd97ebe22f
qazx7412 <git@newgenerations.im>
2018-11-15 12:14:52 +0900

    wip

--- handler.js
+++ handler.js
@@ -3,13 +3,11 @@
 const exec = require('child_process').exec;
 
 module.exports.hello_ruby = (event, context, callback) => {
-  const child = exec('./traveling-ruby/bin/ruby ./src/hello_ruby.rb ' + "'" + JSON.stringify(event) + "'");
-
-  child.stdout.on('data', (result) => {
-    callback(null,result);
-  });
-  child.stderr.on('data', (result) => {
-    callback(result);
+  exec('./traveling-ruby/bin/ruby ./src/hello_ruby.rb ' + "'" + JSON.stringify(event) + "'", (error, stdout, stderr) => {
+    if (error) {
+      callback(error);
+    }
+    callback(stderr,stdout);
   });
 };
 
@@ -23,12 +21,10 @@ module.exports.hello_crystal = (event, context, callback) => {
 };
 
 module.exports.build_elixir = (event, context, callback) => {
-  const child = exec('./buildfile/build_elixir ' + "'" + JSON.stringify(event) + "'");
-
-  child.stdout.on('data', (result) => {
-    callback(null,result);
-  });
-  child.stderr.on('data', (result) => {
-    callback(result);
+  exec('./buildfile/build_elixir ' + "'" + JSON.stringify(event) + "'", (error, stdout, stderr) => {
+    if (error) {
+      callback(error);
+    }
+    callback(stderr,stdout);
   });
 };
