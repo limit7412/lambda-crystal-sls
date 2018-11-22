397c3744f00061474661a1207b07db3028746e82
qazx7412 <git@newgenerations.im>
2018-11-22 17:03:09 +0900

    ignore

--- lib/clim/src/clim/exception.cr
+++
@@ -1,8 +0,0 @@
-class Clim
-  class ClimException < Exception
-  end
-  class ClimInvalidOptionException < Exception
-  end
-  class ClimInvalidTypeCastException < Exception
-  end
-end
--- lib/clim/src/clim/version.cr
+++
@@ -1,3 +0,0 @@
-class Clim
-  VERSION = "0.4.0"
-end
