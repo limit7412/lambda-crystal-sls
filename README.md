f1b13a9379c9125e6a6502cc47d43815b2e8864d
qazx7412 <git@newgenerations.im>
2018-12-04 13:26:30 +0900

    sls

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lambdaでcrystalが動かないかと企んだ残骸
   - Custom Runtimeでうまいこと対応出来れば…
+
+### 参考にした
+  - https://www.m3tech.blog/entry/aws-lambda-custom-runtime
--- src/hello/main.cr
+++ src/hello/main.cr
@@ -1,1 +1,13 @@
-p "test"
+require "http/client"
+
+while true
+  response = HTTP::Client.get "http://#{ENV["AWS_LAMBDA_RUNTIME_API"]}/2018-06-01/runtime/invocation/next"
+  event_data = response.body
+  request_id = response.headers["Lambda-Runtime-Aws-Request-Id"]
+
+  body = event_data
+
+  url : String = "http://#{ENV["AWS_LAMBDA_RUNTIME_API"]}/2018-06-01/runtime/invocation/#{request_id}/response"
+  HTTP::Client.post url, body: body
+end
+
